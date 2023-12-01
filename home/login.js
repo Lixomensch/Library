ed0194d12d2996a7549dbd502d4a5433ae119b64
João augusto silva <136506636+tenma2010@users.noreply.github.com>
2023-11-30 21:00:15 -0300

    Add files via upload

---
+++ home/login.js
@@ -0,0 +1,89 @@
+function abrirTelaFlutuante() {
+  // Abre a tela flutuante ao clicar no botão "Abrir Tela Flutuante"
+  document.getElementById('login').style.display = 'flex';
+}
+function abrirTelaCadastro() {
+  // Fecha a tela flutuante ao clicar no botão "Fechar"
+  document.getElementById('login').style.display = 'none';
+  document.getElementById('login2').style.display = 'flex';
+}
+
+function fecharTelaFlutuante() {
+  // Fecha a tela flutuante ao clicar no botão "Fechar"
+  document.getElementById('login').style.display = 'none';
+}
+
+function fecharTelaCadastro() {
+  // Fecha a tela flutuante ao clicar no botão "Fechar"
+  document.getElementById('login2').style.display = 'none';
+}
+
+async function submitLogin() {
+  var username = document.getElementById('loginusername').value;
+  var password = document.getElementById('loginpassword').value;
+  var loginMessage = document.getElementById('loginMessage');
+
+  document.getElementById('loginusername').value = "";
+  document.getElementById('loginpassword').value = "";
+
+
+  
+  await fetch('/login',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  }) 
+  .then(Response => Response.json())
+  .then(data =>{
+      if (data.success === false)
+      {
+        loginMessage.innerHTML = 'Invalid username or password';
+        loginMessage.style.color = 'red';
+      }
+      alert(data.message);
+  })
+  .catch(error => {
+      console.error("Erro ao enviar solicitação: ", error);
+  })
+  
+}
+
+async function submitCad() {
+  var username = document.getElementById('signupusername').value;
+  var password = document.getElementById('signuppassword').value;
+
+  var loginMessage = document.getElementById('cadMessage');
+
+  document.getElementById('signupusername').value = "";
+  document.getElementById('signuppassword').value = "";
+
+  await fetch('/cadastrar',{
+    method: 'POST',
+    headers:{
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify({
+      username:username,
+      password:password,
+    }),
+})
+  .then(Response=> Response.json())
+  .then(data=>{
+    if(data.success===false){
+      loginMessage.innerHTML = 'Invalid username or password';
+      loginMessage.style.color = 'red';
+    }
+    alert(data.message);
+  })
+  .catch(error => {
+    console.error("Erro ao enviar solicitação: ", error);
+})
+  
+}
+
+
